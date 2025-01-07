5b8a3dbc95ba25d21e4ed6da197dfa44c739c82e
Gustav Dalemo <gustavda@student.chalmers.se>
2025-01-08 00:58:49 +0100

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="app">
+        <h1>{{ message }}</h1>
+        <p>AKmusik under uppbyggnad...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            message: 'Work in Progress'
+        }
+    }
+}
+</script>
+
+<style>
+    #app {
+        text-align: center;
+        padding: 50px;
+    }
+
+    h1 {
+        font-size: 2em;
+        color: #ff6347; /* Tomato color */
+    }
+
+    p {
+        font-size: 1.5em;
+        color: #555; /* Gray color */
+    }
+
+</style>

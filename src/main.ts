e7f5197a8c72ae5c9e45e7b8228c6590caffdeb8
Gustav Dalemo <gustavda@student.chalmers.se>
2025-01-10 22:28:33 +0100

    Imported bootstrap into project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import {createBootstrap} from 'bootstrap-vue-next'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 const app = createApp(App)
 app.use(router)
+app.use(createBootstrap())
 app.mount('#app')
